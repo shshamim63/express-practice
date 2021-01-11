306ffd488789a87085898f90799b99bcef6418a9
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-11 20:02:12 +0600

    Added nodemon and couple of routes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.get('',)
+app.get('', (req, res) => {
+  res.send('Hello')
+})
+
+app.get('/help',(req, res) => {
+  res.send("I am from help")
+})
+
+app.listen(3000, () => {
+  console.log('Server is listening in port 3000')
+})
