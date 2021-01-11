39f8727bd7387ce59e9fc5b25df412e580220133
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-11 20:28:11 +0600

    Added static page to render it.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname,"../public")
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   res.send('Hello')
@@ -9,6 +13,19 @@ app.get('', (req, res) => {
 app.get('/help',(req, res) => {
   res.send("I am from help")
 })
+
+app.get('/weather',(req, res) => {
+  res.send('<h1>Rendering Html</h1>')
+})
+
+
+app.get('/info',(req, res) => {
+  res.send({
+    name: "Shakhawat Hossain",
+    location: "Dhaka, Bangladesh"
+  })
+})
+
 
 app.listen(3000, () => {
   console.log('Server is listening in port 3000')
