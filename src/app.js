a7f935fa97d21bb9c5ce2ea8b03fe7fd0eab4764
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-12 22:12:05 +0600

    Added views handelbars

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,"../public")
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: "New Content",
+    from: 'hbs'
+  })
+})
 
 app.get('/help',(req, res) => {
   res.send("I am from help")
