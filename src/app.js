db1ae2b26d5a4d8bb77d9fa092844db32a3b1c89
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-13 12:11:06 +0600

    Added partial view by configuring hbs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,"../public")
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialviewPath = path.join(__dirname, '../templates/partials')
 
+hbs.registerPartials(partialviewPath)
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath))
 
